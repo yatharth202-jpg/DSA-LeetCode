cc39655ce23d8c40b9239ed7477a6699e678bbad
Yatharth Srivastava <srivastavayatharth1@gmail.com>
2026-07-12 23:41:53 +0530

    Time: 4 ms (100.00%) | Memory: 46 MB (53.61%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,21 +1,20 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int OriginalNum = x;
-        int result = 0;
-
-        while(x>0) {
-    
-        int digit = x % 10 ;
-
-        result = result*10 + digit;
-
-        x = x/10;
-        }
-
-        if(result == OriginalNum) {
-            return true;
-        } else {
+  // Negative numbers and numbers ending with 0 (except 0) are not palindromes
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
+
+        int reversed = 0;
+
+        // Reverse half of the number
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+
+        // For even digits: x == reversed
+        // For odd digits: x == reversed / 10
+        return x == reversed || x == reversed / 10;
 }
 }
