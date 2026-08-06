4a13f54476091d254ea7003c4fb0b0c27f283fb2
Yatharth Srivastava <srivastavayatharth1@gmail.com>
2026-08-06 23:42:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (34.01%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int fib(int n) {
+        if(n == 1) {
+            return 1;
+        }
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        for(int i=2 ; i<=n ; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return c;
+    }
+ }
